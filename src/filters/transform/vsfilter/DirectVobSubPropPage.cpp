bf81ba98651e08283778746e95c32edf5b5451b0
v0lt <mpc-be@mail.ru>
2026-03-24 11:43:25 +0300

    Increased the width of the format list column to accommodate the names of BITF formats.

--- src/filters/transform/vsfilter/DirectVobSubPropPage.cpp
+++ src/filters/transform/vsfilter/DirectVobSubPropPage.cpp
@@ -1451,7 +1451,7 @@ void CDVSColorPPage::UpdateControlData(bool fSave)
         m_outputFmtList.DeleteAllItems();
         m_outputFmtList.DeleteColumn(0);
         m_outputFmtList.SetExtendedStyle( (m_outputFmtList.GetStyle()|LVS_EX_CHECKBOXES) & ~LVS_EX_GRIDLINES );
-        m_outputFmtList.InsertColumn(0, _T("output"), LVCFMT_LEFT, 110);
+        m_outputFmtList.InsertColumn(0, _T("output"), LVCFMT_LEFT, 148);
         for(int i = 0; i < static_cast<int>(m_outputColorSpaceCount); i++)
         {
             m_outputFmtList.InsertItem(i, GetColorSpaceName(m_outputColorSpace[i].color_space,OUTPUT_COLOR_SPACE));
